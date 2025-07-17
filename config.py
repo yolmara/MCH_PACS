30f6855be199bc29d962938eaa1f7f2896ef9569
Coder911 <brandonmasuku911@gmail.com>
2025-07-17 17:07:52 +0200

    Switch to Render DB

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://coder911:coder.03@localhost:5432/pacsdb'
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
