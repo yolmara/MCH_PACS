18149534c7582e8b00b8728f8add5017439d3cc1
Brandon Masuku <brandonmasuku911@gmail.com>
2025-07-20 20:29:06 +0200

    dependancy fix for compatibility

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('coder.03', 'fallback-secret-for-dev')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-for-dev')
     
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///mch_pacs.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
